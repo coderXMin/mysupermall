a30148d2565a99300124bcf07bf60f4350898bc4
wym-520 <84628409+wym-520@users.noreply.github.com>
2021-08-29 15:27:20 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
 
     //1. 创建axios实例
     const instance = axios.create({
-        baseURL: 'http://152.136.185.210:7878/api/m5',
+        baseURL: '',
         timeout: 5000,
     });
 
